8487928026e8df748046ebd75b58230024ff9ae3
Kris Ward <kristopher.ward@tessella.com>
2016-05-20 11:10:54 +0100

    Refactored the find square tests

--- tests/find_square.py
+++ tests/find_square.py
@@ -7,54 +7,80 @@ from dls_barcode.datamatrix import DataMatrix
 from dls_barcode.datamatrix.locate_square import SquareLocator
 
 
-IMG_ROOT_DIR = '../test-output/square_test/bad/'
-OUT_DIR = IMG_ROOT_DIR + 'algorithm_test/'
+class SquareTest:
+    """ Put test cases (images of individual barcodes) in a subdirectory under ROOT_DIR. The first
+    two characters of the file name of each image should be the estimated side length in pixels
+    (01-99) of the barcode.
+
+    The name of the subdirectory should be passed to the constructor as the 'case' argument.
+    """
+    # Prepare wiggles for reading
+    w = 0.25
+    WIGGLES = [[0, 0], [w, w], [-w, -w], [w, -w], [-w, w]]
+
+    ROOT_DIR = '../test-output/square_test/'
+
+    def __init__(self, case, debug):
+        self.case = case
+        self.directory = self.ROOT_DIR + case + "/"
+
+        self.locator = SquareLocator()
+        self.locator.DEBUG = debug
+
+        # Counters
+        self.count_success = 0
+        self.count_iters = 0
+
+    def process_all(self):
+        print("-------- " + self.case + " -------")
+        # Get list of test case files
+        paths = [os.path.join(self.directory, o) for o in os.listdir(self.directory)]
+        good_files = [p for p in paths if os.path.isfile(p)]
+        count_total = len(good_files)
+
+        start_time = time.time()
+        for filepath in good_files:
+            self._process_file(filepath)
+
+        total_time = time.time()-start_time
+
+        if count_total > 0:
+            print("SUCCESSFUL READS - {} / {}".format(self.count_success, count_total))
+            print("TOTAL ITERATIONS = {} ({:.1f} each)".format(self.count_iters, self.count_iters/count_total))
+            print("TIME - {:.2f} ({:.2f} each)".format(total_time, total_time/count_total))
+
+    def _process_file(self, filepath):
+        filename = filepath.split("/")[-1]
+
+        barcode_size = int(filename[:2])
+
+        color = Image(filepath).to_alpha()
+        gray = color.to_grayscale()
+
+        fp = self.locator.locate(gray, barcode_size)
+        barcode = DataMatrix(fp, gray)
+        barcode.perform_read(self.WIGGLES)
+
+        iters = self.locator.count
+        self.count_iters += iters
+
+        print("{} - {} - {}".format(filename, iters, barcode.data()))
+        if barcode.is_valid():
+            self.count_success += 1
+
+        # fp.draw_to_image(color)
+        # color.rescale(2).popup()
 
 
-# Get list of test case files
-paths = [os.path.join(IMG_ROOT_DIR, o) for o in os.listdir(IMG_ROOT_DIR)]
-good_files = [p for p in paths if os.path.isfile(p)]
+def main():
+    good_tester = SquareTest("good", False)
+    good_tester.process_all()
 
-# Prepare locator object
-locator = SquareLocator()
-locator.DEBUG = True
-
-# Prepare wiggles for reading
-w = 0.25
-wiggle_offsets = [[0, 0], [w, w], [-w, -w], [w, -w], [-w, w]]
-
-# Counters
-good_total = len(good_files)
-good_success = 0
-good_iter_count = 0
-start_time = time.time()
+    bad_tester = SquareTest("bad", False)
+    bad_tester.process_all()
 
 
-for filepath in good_files:
-    filename = filepath.split("/")[-1]
-
-    barcode_size = int(filename[:2])
-    rotation = 0
-
-    color = Image(filepath).to_alpha()
-    gray = color.to_grayscale()
-
-    fp = locator.locate(gray, barcode_size)
-    barcode = DataMatrix(fp, gray)
-    barcode.perform_read(wiggle_offsets)
-
-    iters = locator.count
-    good_iter_count += iters
-
-    print("{} - {} - {}".format(filename, iters, barcode.data()))
-    if barcode.is_valid():
-        good_success += 1
+if __name__ == '__main__':
+    main()
 
 
-    fp.draw_to_image(color)
-    #color.rescale(2).popup()
-
-total_time = time.time()-start_time
-print("GOOD READS - {} / {}".format(good_success, good_total))
-print("TOTAL ITERATIONS = {} ({:.1f} per)".format(good_iter_count, good_iter_count/good_total))
-print("TIME - {:.2f} ({:.2f} per)".format(total_time, total_time/good_total))
