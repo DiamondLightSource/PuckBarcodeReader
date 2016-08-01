0e5c2d7658fc4f5de4df531c79aa30889bb6cda5
Kris Ward <kristopher.ward@tessella.com>
2016-08-01 12:25:35 +0100

    New circle detector utility class

---
+++ dls_barcode/util/cv/__init__.py
@@ -0,0 +1,1 @@
+from .circle_detector import CircleDetector
---
+++ dls_barcode/util/cv/circle_detector.py
@@ -0,0 +1,59 @@
+import cv2
+
+from util.shape import Point, Circle
+
+_OPENCV_MAJOR = cv2.__version__[0]
+
+if _OPENCV_MAJOR == '3':
+    _HOUGH_METHOD = cv2.HOUGH_GRADIENT
+else:
+    from cv2 import cv
+    _HOUGH_METHOD = cv.CV_HOUGH_GRADIENT
+
+
+class CircleDetector:
+    def __init__(self):
+        self._canny_thresh = 150
+        self._accumulator_thresh = 100
+        self._dp = 2
+        self._min_dist = 100
+        self._min_radius = 100
+        self._max_radius = 150
+        self._method = _HOUGH_METHOD
+
+    def set_canny_threshold(self, value): self._canny_thresh = value
+
+    def set_accumulator_threshold(self, value): self._accumulator_thresh = value
+
+    def set_dp(self, value): self._dp = value
+
+    def set_minimum_separation(self, value): self._min_dist = value
+
+    def set_minimum_radius(self, value): self._min_radius = value
+
+    def set_maximum_radius(self, value): self._max_radius = value
+
+    def find_circles(self, mono_img):
+        import time
+        start = time.time()
+
+        raw_circles = cv2.HoughCircles(image=mono_img.img, method=self._method, dp=self._dp,
+                                       minDist=self._min_dist, param1=self._canny_thresh,
+                                       param2=self._accumulator_thresh, minRadius=int(self._min_radius),
+                                       maxRadius=int(self._max_radius))
+
+        print("time: {:.3f}".format(time.time() - start))
+
+        circles = self._sanitize_circles(raw_circles)
+        return circles
+
+    @staticmethod
+    def _sanitize_circles(raw_circles):
+        circles = []
+        if raw_circles is not None:
+            for raw in raw_circles[0]:
+                center = Point(int(raw[0]), int(raw[1]))
+                radius = int(raw[2])
+                circles.append(Circle(center, radius))
+
+        return circles
