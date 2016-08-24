b6f67222e1d7fb079295781781477dfb799cdceb
Kris Ward <kristopher.ward@tessella.com>
2016-08-24 17:00:13 +0100

    Blank geometry can now be serialized

--- dls_barcode/plate/geometry/blank.py
+++ dls_barcode/plate/geometry/blank.py
@@ -1,15 +1,42 @@
+from util.shape import Circle
+
+
 class BlankGeometry:
+    _SERIAL_DELIM = "-"
+
     TYPE_NAME = "None"
     NUM_SLOTS = 20
 
     def __init__(self, barcodes):
-        self._slot_bounds = []
-        self._set_slot_bounds(barcodes)
+        self._barcode_bounds = []
+        self._set_barcode_bounds(barcodes)
 
-    def _set_slot_bounds(self, barcodes):
-        self._slot_bounds = []
+    def _set_barcode_bounds(self, barcodes):
+        self._barcode_bounds = []
         for barcode in barcodes:
-            self._slot_bounds.append(barcode.bounds())
+            self._barcode_bounds.append(barcode.bounds())
 
     def slot_bounds(self, slot_num):
-        return self._slot_bounds[slot_num - 1]
+        return self._barcode_bounds[slot_num - 1]
+
+    ############################
+    # Serialization
+    ############################
+    @staticmethod
+    def deserialize(string):
+        circle_strs = string.split(BlankGeometry._SERIAL_DELIM)
+
+        barcode_bounds = []
+        for string in circle_strs:
+            circle = Circle.deserialize(string)
+            barcode_bounds.append(circle)
+
+        geo = BlankGeometry([])
+        geo._barcode_bounds = barcode_bounds
+        return geo
+
+    def serialize(self):
+        circles = []
+        for bounds in self._barcode_bounds:
+            circles.append(bounds.serialize())
+        return self._SERIAL_DELIM.join(circles)
