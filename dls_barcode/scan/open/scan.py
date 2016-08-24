5d70c5eee82c6d0084449db165441556c4055ce0
Kris Ward <kristopher.ward@tessella.com>
2016-08-24 13:34:13 +0100

    Created new open scanner that looks for all barcodes in an image without regard for geometry

---
+++ dls_barcode/scan/open/scan.py
@@ -0,0 +1,45 @@
+from dls_barcode.datamatrix import DataMatrix, Locator
+
+
+from .result import OpenScanResult
+
+
+class NoBarcodesError(Exception):
+    pass
+
+
+class OpenScanner:
+    def __init__(self):
+        self._frame_img = None
+
+    def scan_frame(self, frame_img):
+        self._frame_img = frame_img
+        result = OpenScanResult()
+        result.start_timer()
+
+        try:
+            barcodes = self._perform_frame_scan()
+            result.set_barcodes(barcodes)
+
+        except NoBarcodesError as ex:
+            result.set_error(str(ex))
+
+        result.end_timer()
+        return result
+
+    def _perform_frame_scan(self):
+        barcodes = self._locate_all_barcodes_in_image()
+
+        for barcode in barcodes:
+            barcode.perform_read(DataMatrix.DIAG_WIGGLES)
+
+        return barcodes
+
+    def _locate_all_barcodes_in_image(self):
+        #todo: use deep contour locator
+        barcodes = DataMatrix.LocateAllBarcodesInImage(self._frame_img)
+
+        if len(barcodes) == 0:
+            raise NoBarcodesError("No Barcodes Detected In Image")
+
+        return barcodes
