e3d1af9136fd9caf2e4a797dd7330b9f5f6e5469
rqq82173 <urszula.neuman@gmail.com>
2017-02-13 19:41:52 +0000

    #4 Improve test coverage of the code
    image test - several new tests added

--- tests/unit_tests/dls_util/image/test_image.py
+++ tests/unit_tests/dls_util/image/test_image.py
@@ -1,18 +1,35 @@
 import unittest
-import cv2
+import numpy as np
 
 from dls_util.image import Image
 
 
 class TestImage(unittest.TestCase):
 
-    def test_is_valid_returns_correct_value(self):
-        image = Image(cv2.imread('puck_test.png'))
-        self.assertNotEqual(image.width, 0)
-        self.assertNotEqual(image.height, 0)
-        self.assertTrue(image.is_valid())
+    def test_paste_when_location_is_outside_image_x_off(self):
+        image_target = Image.blank(10, 10)
+        image_src = Image(img=np.full((5, 5, 3), 2, np.uint8))
+        image_target.paste(image_src, 20, 0)
+        a = np.sum(image_target.img)
+        self.assertEquals(a, 0)
 
-    #def test_paste
+    def test_paste_when_location_is_outside_image_y_off(self):
+        image_target = Image.blank(10, 10)
+        image_src = Image(img=np.full((5, 5, 3), 2, np.uint8))
+        image_target.paste(image_src, 0, 20)
+        a = np.sum(image_target.img)
+        self.assertEquals(a, 0)
+
+    def test_paste_when_location_is_inside_image_3_channels(self):
+        image_target = Image.blank(10, 10)
+        image_src = Image(img=np.full((1, 1, 3), 2, np.uint8))
+        image_target.paste(image_src, 0, 0)
+        a = np.sum(image_target.img)
+        self.assertEquals(a, 6)
+        self.assertEquals(image_target.img[0, 0, 1], 2)
+        self.assertEquals(image_target.img[0, 1, 1], 0)
+
+    #def test_paste_when_location_is_inside_image_4_channels(self):
 
     #def test_sub_image
 
