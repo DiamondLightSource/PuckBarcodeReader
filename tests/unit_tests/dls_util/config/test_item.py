922f3cb992d0a61423afd2dd2d7398c35ddca196
rqq82173 <urszula.neuman@gmail.com>
2017-04-27 17:02:35 +0100

    #4 Improve test coverage of the code
    item test - continuation

--- tests/unit_tests/dls_util/config/test_item.py
+++ tests/unit_tests/dls_util/config/test_item.py
@@ -1,25 +1,14 @@
 import unittest
-from dls_util import ConfigItem, IntConfigItem
+from dls_util import ConfigItem, IntConfigItem, DirectoryConfigItem, BoolConfigItem
 
 class TestItem(unittest.TestCase):
 
-    def test_init_creates_empty_vale(self):
-        item = ConfigItem("test_int", 2)
-        self.assertEquals(item._value, None)
 
-    def test_init_tag_is_correct(self):
-        item = ConfigItem("test_int", 2)
-        self.assertEquals(item._tag, "test_int")
-
-    def test_init_default_is_correct(self):
-        item = ConfigItem("test_int", 2)
-        self.assertEquals(item._default, 2)
-
-    def test_value_returns_correctly(self):
+    def test_value_returns_None_If_value_not_set(self):
         item = ConfigItem("test_int", 2)
         self.assertEquals(item.value(), None)
 
-    def test_set_sets_correct_value(self):
+    def test_if_set_and_returned_values_are_the_same(self):
         item = ConfigItem("test_int", 2)
         item.set(10)
         self.assertEquals(item.value(), 10)
@@ -43,22 +32,56 @@ class TestItem(unittest.TestCase):
 
 class TestIntConfigItem(unittest.TestCase):
 
-    def test_units_set_correctly_by_ini(self):
-        item = IntConfigItem("test_int", 2, 'mm')
-        self.assertEquals(item._units, 'mm')
-
-    def test_unit_returns_correct_value(self):
+    def test_units_returns_cam_if_set_to_cm_in_constructor(self):
         item = IntConfigItem("test_int", 2, 'cm')
         self.assertEquals(item.units(), 'cm')
 
-    def test_clean_returns_default_value_if_not_int(self):
+    def test_from_file_string_sets_default_value_if_not_int(self):
         item = IntConfigItem("test_int", 2, 'cm')
-        self.assertEquals(item._clean("buu"), 2)
+        item.from_file_string("buu")
+        self.assertEquals(item.value(), 2)
 
-    def test_clean_returns_int_value_correctly(self):
+    def test_from_file_string_sets_int_value_correctly(self):
         item = IntConfigItem("test_int", 2, 'cm')
-        self.assertEquals(item._clean("20"), 20)
+        item.from_file_string("20")
+        self.assertEquals(item.value(), 20)
 
+class TestDirectoryConfigItem(unittest.TestCase):
+
+    def test_from_file_string_sets_value_as_string_finished_with_slash(self):
+        item = DirectoryConfigItem("test_int", 15)
+        item.from_file_string("20")
+        self.assertEquals(item.value(), "20/")
+
+        item = DirectoryConfigItem("test_int", 15)
+        item.from_file_string(20)
+        self.assertEquals(item.value(), "20/")
+
+        item = DirectoryConfigItem("test_int", 15)
+        item.from_file_string("test")
+        self.assertEquals(item.value(), "test/")
+
+        item = DirectoryConfigItem("test_int", 15)
+        item.from_file_string("test/")
+        self.assertEquals(item.value(), "test/")
+
+class TestBoolConfigItem(unittest.TestCase):
+
+    def test_from_file_string_sets_value_True_when_input_string_is_true(self):
+        item = BoolConfigItem("test_int", 15)
+        item.from_file_string("TrUE")
+        self.assertTrue(item.value())
+
+        item.from_file_string("trUE")
+        self.assertTrue(item.value())
+
+    def test_from_file_string_sets_value_True_when_input_string_is_other_than_True(self):
+        item = BoolConfigItem("test_int", 15)
+        item.from_file_string("AAAAA")
+        self.assertFalse(item.value())
+
+
+class TestEnumConfigItem(unittest.TestCase):
 
 
 
