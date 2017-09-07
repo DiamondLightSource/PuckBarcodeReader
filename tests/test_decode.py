d0e3597d9a2fcf710a182fd6d20618affcbcc5ce
Isabella Rey <reyi@tessella.co.uk>
2017-09-07 10:55:31 +0100

    Jira Task I04_1-148: fixed broken unit test

--- tests/test_decode.py
+++ tests/test_decode.py
@@ -6,7 +6,7 @@ cases = [
     ([85, 102, 116, 117, 129], "Test"),
     ([85, 102, 116, 117, 129, 101, 102, 103], "Test"),
     ([69, 71, 145, 49, 70, 134, 173, 129], "DF150E0443"),
-    ([x+1 for x in range(32, 127)], str(bytearray([x for x in range(32, 127)]))) #, 'utf-8'
+    ([x+1 for x in range(32, 127)], bytearray([x for x in range(32, 127)]).decode("utf-8")) #, 'utf-8'
 ]
 
 
