132954517b3782dbec2a160e2403f6645eee087e
rqq82173 <urszula.neuman@diamond.ac.uk>
2018-05-01 10:40:39 +0100

    Problem with accessing fit.png from exe. The exe has to be released with resources/features/fit.png.

--- build.bat
+++ build.bat
@@ -3,6 +3,9 @@ mkdir bin
 
 cd dls_barcode
 
+REM use to debug the exe file
+REM pyinstaller --onefile --icon=..\resources\icons\qr_code.ico --debug --clean main.py
+
 pyinstaller --onefile --windowed --icon=..\resources\icons\qr_code.ico --clean main.py
 
 move dist\main.exe ..\bin\barcode.exe
