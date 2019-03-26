3e99d46e25f07255f41958a24d02a5b91d50f035
rqq82173 <urszula.neuman@diamond.ac.uk>
2019-03-26 15:38:32 +0000

    code documentation and the build file update

--- build.bat
+++ build.bat
@@ -6,11 +6,9 @@ cd dls_barcode
 REM use to debug the exe file
 REM pyinstaller --onefile --icon=..\resources\icons\qr_code.ico --debug --clean main.py
 
-pyinstaller --onefile --windowed --icon=..\resources\icons\qr_code.ico --clean main.py
+pyinstaller --onefile --windowed --icon="C:\Users\rqq82173\PycharmProjects\PuckBarcodeReader\resources\icons\qr_code.ico" --add-data "C:\Users\rqq82173\PycharmProjects\PuckBarcodeReader\resources; C:\Users\rqq82173\PycharmProjects\PuckBarcodeReader\bin" --clean "C:\Users\rqq82173\PycharmProjects\PuckBarcodeReader\dls_barcode\main.py"
 
 move dist\main.exe ..\bin\barcode.exe
 rd /S /Q build
 rd /S /Q dist
 del main.spec
-
-@echo start barcode.exe --config_file .\config.ini>"..\bin\launch_barcode.bat"
