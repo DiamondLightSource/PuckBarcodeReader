e70fe4b252938c38aad744cd54b48fe69a35bb09
rqq82173 <urszula.neuman@diamond.ac.uk>
2019-04-01 13:31:52 +0100

    backup - backup test simplified

--- tests/unit_tests/test_dls_barcode/test_data_store/test_backup.py
+++ tests/unit_tests/test_dls_barcode/test_data_store/test_backup.py
@@ -4,24 +4,25 @@ import time
 from mock import MagicMock
 
 from dls_barcode.data_store.backup import Backup
-from dls_barcode.data_store.record import Record
-
-ID0 = "id0"
-ID1 = "id1"
-ID2 = "id2"
-ID3 = "id3"
 
 class TestBackup(unittest.TestCase):
 
     def setUp(self):
         self.comms_man = MagicMock()
         self.backup_time = MagicMock()
-        self.backup_time.value.return_value = 3
+        weeks = 3
+        WEEK_IN_SECONDS = 604800
+        self.backup_time.value.return_value = weeks
+        record_one = MagicMock(timestamp=time.time() - (weeks-1)*WEEK_IN_SECONDS)
+        record_two = MagicMock(timestamp=time.time() - weeks*WEEK_IN_SECONDS)
+        record_three = MagicMock(timestamp=time.time() - (weeks+1)*WEEK_IN_SECONDS)
+        record_three.to_csv_string.return_value = 'csv_string_three'
+        record_three.to_string.return_value = 'string_three'
+        self.records = [record_one, record_two, record_three]
 
     def test_truncate_removes_all_old_records(self):
         # Arrange
-        records = self._get_records()
-        self.comms_man.load_records_from_file.return_value = records
+        self.comms_man.load_records_from_file.return_value = self.records
 
         # Act
         backup = Backup(self.comms_man, self.backup_time)
@@ -31,33 +32,29 @@ class TestBackup(unittest.TestCase):
         # Assert
         new_size = len(backup._records)
         self.assertNotEqual(orig_size, new_size)
-        self.assertEqual(new_size, 0)
+        self.assertEqual(new_size, 2)
 
     def test_backup_records_adds_new_records_to_existing_list(self):
         # Arrange
-        records = self._get_records()
-        self.comms_man.load_records_from_file.return_value = records
+        self.comms_man.load_records_from_file.return_value = self.records
 
         # Act
         backup = Backup(self.comms_man, self.backup_time)
-        org_size = len(backup._records)
-        backup._truncate_record_list = MagicMock() #mute truncate for the test
-        backup.backup_records(records)
+        backup.backup_records(self.records)
 
         # Assert
-        self.assertEqual(len(backup._records), 2*org_size)
+        self.assertEqual(len(backup._records), 4) #old truncated
 
-    def test_backup_records_removes_old_records_when_its_called(self):
+    def test_backup_records_removes_old_records_from_the_passed_when_its_called(self):
         # Arrange
-        records = self._get_records()
-        self.comms_man.load_records_from_file.return_value = records
+        self.comms_man.load_records_from_file.return_value = []
 
         # Act
         backup = Backup(self.comms_man, self.backup_time)
-        backup.backup_records(records)
+        backup.backup_records(self.records)
 
         # Assert
-        self.assertEqual(len(backup._records), 0)
+        self.assertEqual(len(backup._records), 2)
 
     def test_backup_records_to_csv_file(self):
         #Arrange
@@ -72,10 +69,7 @@ class TestBackup(unittest.TestCase):
 
     def test_is_old_returns_true_for_records_older_than_assumed_number_of_weeks(self):
         # Arrange
-        weeks = self.backup_time.value.return_value
-        timestamp = str(time.time() - 604800 * (weeks + 1))
-        rec_string = "f59c92c1;" + timestamp + ";test.png;None;ABCDE123,ABCDE123;1569:1106:70-2307:1073:68-1944:1071:68"
-        record = Record.from_string(rec_string)
+        record = self.records[2]
 
         # Act
         backup = Backup(self.comms_man, self.backup_time)
@@ -85,28 +79,10 @@ class TestBackup(unittest.TestCase):
 
     def test_is_old_returns_false_for_records_younger_than_assumed_number_of_weeks(self):
         # Arrange
-        weeks = self.backup_time.value.return_value
-        timestamp = str(time.time() - 604800 * (weeks - 1))
-        rec_string = "f59c92c1;"+timestamp+";test.png;None;ABCDE123,ABCDE123;1569:1106:70-2307:1073:68-1944:1071:68"
-        record = Record.from_string(rec_string)
+        record = self.records[0]
 
         # Act
         backup = Backup(self.comms_man, self.backup_time)
 
         # Assert
         self.assertFalse(backup._is_old(record))
-
-    def _get_record_strings(self):
-        str_rep = list()
-        str_rep.append(ID0 + ";1494238923.0;test" + ID0 + ".png;None;DLSL-001,DLSL-010,DLSL-011,DLSL-012;1569:1106:70-2307:1073:68-1944:1071:68")
-        str_rep.append(ID1 + ";1494238922.0;test" + ID1 + ".png;None;DLSL-002,DLSL-010,DLSL-011,DLSL-012;1569:1106:70-2307:1073:68-1944:1071:68")
-        str_rep.append(ID2 + ";1494238921.0;test" + ID2 + ".png;None;DLSL-003,DLSL-010,DLSL-011,DLSL-012;1569:1106:70-2307:1073:68-1944:1071:68")
-        str_rep.append(ID3 + ";1494238920.0;test" + ID3 + ".png;None;DLSL-004,DLSL-010,DLSL-011,DLSL-012;1569:1106:70-2307:1073:68-1944:1071:68")
-        return str_rep
-
-    def _get_records(self):
-        strings = self._get_record_strings()
-        rep = list()
-        for str in strings:
-            rep.append(Record.from_string(str))
-        return rep
