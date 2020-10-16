17af2c711acb678c98c7b280bcf74aeebec5668c
Emma Dixon <emma.dixon@diamond.ac.uk>
2020-10-16 17:45:25 +0100

    Edit record table for sessions

--- dls_barcode/gui/record_table.py
+++ dls_barcode/gui/record_table.py
@@ -1,16 +1,21 @@
 from __future__ import division
 
+from datetime import datetime
 from PyQt5 import QtWidgets
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtWidgets import QGroupBox, QVBoxLayout, QHBoxLayout, QTableWidget, QMessageBox
 
 from dls_barcode.data_store import Store
 from dls_barcode.data_store.store_loader import StoreLoader
 from dls_barcode.data_store.store_writer import StoreWriter
+from dls_barcode.data_store.session_manager import SessionManager
 
 # todo: allow delete key to be used for deletion
 # todo: allow record selection with arrow keys
 
+RED = "; color: red"
+BLACK = "; color: black"
+BASIC_STYLE_SHEET = "font-size: 12pt"
 
 class ScanRecordTable(QGroupBox):
     """ GUI component. Displays a list of previous scan results. Selecting a scan causes
@@ -28,6 +33,10 @@ class ScanRecordTable(QGroupBox):
 
         self._store = Store(store_writer, store_loader.load_records_from_file())
         self._options = options
+
+        # Create the session  manager and a file writer for sessions
+        session_writer = StoreWriter(options.get_session_directory(), "session")
+        self._session_manager = SessionManager(session_writer, self._store)
 
         self._barcodeTable = barcode_table
         self._imageFrame = image_frame
@@ -49,6 +58,33 @@ class ScanRecordTable(QGroupBox):
 
         self._table.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
 
+        #Session
+        self._current_session_id_label = QtWidgets.QLabel()
+        self._current_session_id_label.setStyleSheet(BASIC_STYLE_SHEET + RED)
+        self._new_session_button = QtWidgets.QPushButton("New Session")
+        self._new_session_button.setStatusTip("Create a new session")
+        self._new_session_button.clicked.connect(self._new_session_clicked)
+        self._end_session_button = QtWidgets.QPushButton("End Session")
+        self._end_session_button.setStatusTip("End the current session")
+        self._end_session_button.clicked.connect(self._end_session_clicked)
+        self._save_session_button = QtWidgets.QPushButton("Save Session")
+        self._save_session_button.setStatusTip("Save the current session")
+        self._save_session_button.clicked.connect(self._save_session_clicked)
+
+        self._new_session_button.setMaximumWidth(100)
+        self._end_session_button.setMaximumWidth(100)
+        self._save_session_button.setMaximumWidth(100)
+
+        vbox = QVBoxLayout()
+
+        hbox = QHBoxLayout()
+        hbox.setSpacing(0)
+        hbox.addWidget(self._new_session_button)
+        hbox.addWidget(self._save_session_button)
+        hbox.addWidget(self._end_session_button)
+        hbox.addStretch(1)
+        hbox.addWidget(self._current_session_id_label)
+        vbox.addLayout(hbox)
 
         # Delete button - deletes selected records
         btn_delete = QtWidgets.QPushButton('Delete')
@@ -61,11 +97,11 @@ class ScanRecordTable(QGroupBox):
         hbox.addWidget(btn_delete)
         hbox.addStretch(1)
 
-        vbox = QVBoxLayout()
         vbox.addWidget(self._table)
         vbox.addLayout(hbox)
 
         self.setLayout(vbox)
+        self._update_session()
 
     def cell_pressed_action_triggered(self, to_run_on_table_clicked):
         self._table.cellPressed.connect(to_run_on_table_clicked)
@@ -81,30 +117,33 @@ class ScanRecordTable(QGroupBox):
     def _load_store_records(self):
         """ Populate the record table with all of the records in the store.
         """
+        start_time = self._session_manager.current_session_id
         self._table.clearContents()
-        self._table.setRowCount(self._store.size())
+        self._table.setRowCount(self._store.size(start_time))
 
         for n, record in enumerate(self._store.records):
-            items = [record.date, record.time, record.holder_barcode, record.num_valid_barcodes,
-                     record.num_unread_slots, record.num_empty_slots, record.plate_type]
-            valid_empty = record.num_valid_barcodes + record.num_empty_slots
-            if valid_empty == record.num_slots:
-                color = self._options.col_ok()
-            elif valid_empty < record.num_slots and record.num_valid_barcodes > 0:
-                color = self._options.col_accept()
-            else:
-                color = self._options.col_bad()
+            if start_time < record.timestamp:
+                items = [record.date, record.time, record.holder_barcode, record.num_valid_barcodes,
+                        record.num_unread_slots, record.num_empty_slots, record.plate_type]
+                valid_empty = record.num_valid_barcodes + record.num_empty_slots
+                if valid_empty == record.num_slots:
+                    color = self._options.col_ok()
+                elif valid_empty < record.num_slots and record.num_valid_barcodes > 0:
+                    color = self._options.col_accept()
+                else:
+                    color = self._options.col_bad()
 
-            color.a = 192
-            for m, item in enumerate(items):
-                new_item = QtWidgets.QTableWidgetItem(str(item))
-                new_item.setBackground(color.to_qt())
-                new_item.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
-                self._table.setItem(n, m, new_item)
+                color.a = 192
+                for m, item in enumerate(items):
+                    new_item = QtWidgets.QTableWidgetItem(str(item))
+                    new_item.setBackground(color.to_qt())
+                    new_item.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+                    self._table.setItem(n, m, new_item)
 
-        # Display the first (most recent) record
-        self._table.setCurrentCell(0, 0)
-        self._record_selected()
+            # Display the first (most recent) record
+            self._table.setCurrentCell(0, 0)
+            self._record_selected()
+
 
     def _record_selected(self):
         """ Called when a row is selected, causes details of the selected record to be
@@ -145,7 +184,36 @@ class ScanRecordTable(QGroupBox):
 
             self._load_store_records()
 
+    def _update_session(self):
+        if self._session_manager.current_session_id:
+            self._current_session_id_label.setStyleSheet(BASIC_STYLE_SHEET + BLACK)
+            self._current_session_id_label.setText(
+                "Session active: {}".format(self._session_manager.current_session_timestamp)
+            )
+        else:
+            self._current_session_id_label.setStyleSheet(BASIC_STYLE_SHEET + RED)
+            self._current_session_id_label.setText("No active session, showing all results in store")
+        self._imageFrame.clear_frame("")
+        self._load_store_records()
+
+    def _new_session_clicked(self):
+        """Called when the 'New Session' button is clicked. Starts a new session"""
+        self._new_session_button.setEnabled(False)
+        QTimer.singleShot(1000, lambda: self._new_session_button.setDisabled(False))
+        self._session_manager.new_session()
+        self._update_session()
+
+    def _end_session_clicked(self):
+        """Called when the 'End Session' button is clicked. Ends the active session"""
+        self._session_manager.end_session()
+        self._update_session()
+
+    def _save_session_clicked(self):
+        """Called when the 'Save Session' button is clicked.
+        Saves csv file with records in current session"""
+        self._session_manager.save_session()
+
     def is_latest_holder_barcode(self, holder_barcode):
-        return self._store.is_latest_holder_barcode(holder_barcode)
+        return self._store.is_latest_holder_barcode(holder_barcode, self._session_manager.current_session_id)
 
 
