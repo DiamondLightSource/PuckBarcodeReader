5315a317c7cd3d3a378a8670f01652427474c0d3
rqq82173 <urszula.neuman@diamond.ac.uk>
2022-03-03 16:54:25 +0000

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,14 +1,12 @@
 rd /S /Q bin
 mkdir bin
 
-cd dls_barcode
-
 REM use to debug the exe file
 REM pyinstaller --onefile --icon=..\resources\icons\qr_code.ico --debug --clean main.py
 
-pyinstaller --onefile --windowed --icon="C:\Users\rqq82173\PycharmProjects\PuckBarcodeReader\resources\icons\qr_code.ico" --clean "C:\Users\rqq82173\PycharmProjects\PuckBarcodeReader\dls_barcode\main.py"
+pyinstaller --onefile --windowed --icon="resources\icons\qr_code.ico" --clean "main.py"
 
-move dist\main.exe ..\bin\barcode.exe
+move dist\main.exe bin\barcode.exe
 rd /S /Q build
 rd /S /Q dist
 del main.spec
