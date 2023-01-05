5455f34a3e5f747ca9584588e999daf1da3a4815
rqq82173 <urszula.neuman@diamond.ac.uk>
2023-01-05 12:27:56 +0000

    Manually point the installer at the dll in venv

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ mkdir bin
 REM use to debug the exe file
 REM pyinstaller --onefile --icon=..\resources\icons\qr_code.ico --debug --clean main.py
 
-pyinstaller --onefile --windowed --icon="resources\icons\qr_code.ico" --clean "main.py"
+pyinstaller --onefile --add-binary "C:\Users\rqq82173\.virtualenvs\PuckBarcodeReader-6rTXUiM6\Lib\site-packages\pylibdmtx\libdmtx-64.dll;." --windowed --icon="resources\icons\qr_code.ico" --clean "main.py"
 
 move dist\main.exe bin\barcode.exe
 rd /S /Q build
